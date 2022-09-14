69aafbe8e1578483f8f63ca28f7a4232edda5be7
Bharath <bharathsurya021@gmail.com>
2022-09-14 18:04:12 +0530

    fn declaration & expressions

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-//function can be used to reuse piece of code, also receive and return data back
+//function can be used to reuse piece of code, also receive and return data back. Allows us to write maintainable code
 function logger() {
     //function body
     console.log('My name is bharath');
@@ -18,3 +18,26 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(2, 4)
 console.log(appleOrangeJuice);
+
+// function declaration and expressions work same way
+
+//function declaration
+const age1 = calcAge1(1996)
+function calcAge1(birthYear) {
+    return 2022 - birthYear;
+}
+console.log(age1);
+
+// fn declaration can be called before they are defined coz fn declarations are hoisted.
+
+//function expression - should be called after initialization
+
+//const age2 = calcAge2(1997) // error: can't access before initialization
+const calcAge2 = function (birthYear) {
+    return 2022 - birthYear
+}
+
+//console.log(calcAge2); // stores the function expression
+const age2 = calcAge2(1997)
+console.log(age2);
+
