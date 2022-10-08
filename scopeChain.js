defbb2b066041f8e1ba8de1613436ee4712c3c30
Bharath <bharathsurya021@gmail.com>
2022-10-08 14:40:04 +0530

    scope chain

---
+++ scopeChain.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function calcAge(birthyear) {
+    const age = 2037 - birthyear
+    console.log(firstName);
+    return age
+}
+
+const firstName = 'bharath'
+
+console.log(
+    calcAge(1996)
+);
