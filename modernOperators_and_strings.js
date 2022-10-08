44383d5a2f0f3396d3867bc34f509effc7abc9f7
Bharath <bharathsurya021@gmail.com>
2022-10-08 15:13:18 +0530

    nested destructuring and set default values

--- modernOperators_and_strings.js
+++ modernOperators_and_strings.js
@@ -6,31 +6,55 @@ const restaurant = {
     categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
     starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+    }
 }
 
-const arr = [2, 3, 4]
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
+console.log(restaurant.order(2, 0))
 
-const [x, y, z] = arr // destructuring of an array
-console.log(x, y, z);
-console.log(arr)
+const [starter, main] = restaurant.order(2, 0)
+console.log(starter, main)
 
-const [first, second] = restaurant.categories
-console.log(first, second) // first and second element of category array
-const [firstel, , , thirdel] = restaurant.categories // leave an empty place to skip through and give us the third element
-console.log(firstel, thirdel) // first and third element of category array
+const nested = [2, 4, [5, 6]]
 
-let [main, , secondary] = restaurant.categories
-console.log(main, secondary);
+const [a, , b] = nested
 
-//switching variables
-// const temp = main
-// main = secondary
-// secondary = temp
+console.log(a, b);
 
-// console.log(main, secondary) change main and secondary category
+// nested destructuring
+const [i, , [j, k]] = nested
+console.log(i, j, k)
 
-[main, secondary] = [secondary, main] //switching by destructuring
-console.log(main, secondary)
+//default values
+
+const [p = 1, q = 1, r = 1] = [8, 9] // setting default values a 1
+console.log(p, q, r)
+
+// const arr = [2, 3, 4]
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+
+// const [x, y, z] = arr // destructuring of an array
+// console.log(x, y, z);
+// console.log(arr)
+
+// const [first, second] = restaurant.categories
+// console.log(first, second) // first and second element of category array
+// const [firstel, , , thirdel] = restaurant.categories // leave an empty place to skip through and give us the third element
+// console.log(firstel, thirdel) // first and third element of category array
+
+// let [main, , secondary] = restaurant.categories
+// console.log(main, secondary);
+
+// //switching variables
+// // const temp = main
+// // main = secondary
+// // secondary = temp
+
+// // console.log(main, secondary) change main and secondary category
+
+// [main, secondary] = [secondary, main] //switching by destructuring
+// console.log(main, secondary)
