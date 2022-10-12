131bf2aac5f88f0e48ccdad42ee39fc4777eaf2c
Bharath <bharathsurya021@gmail.com>
2022-10-12 12:17:12 +0530

    Rest operator

--- modernOperators_and_strings.js
+++ modernOperators_and_strings.js
@@ -30,6 +30,9 @@ const restaurant = {
     },
     orderPasta: function (ing1, ing2, ing3) {
         console.log(`Here is your delicious pasts made with ${ing1},${ing2} and ${ing3}`);
+    },
+    orderPizza: function (mainIngredients, ...otherIngredients) {
+        console.log(otherIngredients)
     }
 }
 
@@ -144,3 +147,36 @@ console.log(restaurantCopy)
 
 // [main, secondary] = [secondary, main] //switching by destructuring
 // console.log(main, secondary)
+
+//Rest operator in destructuring
+
+const myarr = [1, 2, ...[3, 4]] //spread rhs of =
+
+const [a, b, ...others] = [1, 2, 3, 4, 5]
+console.log(a, b, others)// rest on lhs of =
+
+const [pizza, , risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu]
+
+console.log(pizza, risotto, otherFood)
+
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays)
+
+//rest operator in functions
+
+const add = function (...numbers) { // rest parameters
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i]
+    }
+
+    console.log(sum)
+}
+add(2, 3)
+add(5, 3, 4, 7)
+
+const x = [23, 5, 7]
+add(...x)
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach')
+restaurant.orderPizza('mushrooms')
