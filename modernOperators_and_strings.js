412c33857e60bb37fd1e508b0df6e426c88abc23
Bharath <bharathsurya021@gmail.com>
2022-10-19 11:39:19 +0530

    maps & map iterators

--- modernOperators_and_strings.js
+++ modernOperators_and_strings.js
@@ -301,3 +301,74 @@ const staff = ['Waiter', 'Chef', 'Manager', 'Waiter', 'Chef']
 const staffUniq = new Set(staff)
 const staffUnix = [...new Set(staff)]
 console.log(staffUniq, staffUnix)
+
+//Maps
+
+const rest = new Map()
+rest.set('name', 'Classico Italiano')
+rest.set(1, 'Firenze, Italy')
+console.log(rest.set(2, 'Lisbon, Portugal')
+)
+
+rest
+    .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+    .set('open', 11)
+    .set('close', 23)
+    .set(true, 'We are open')
+    .set(false, 'We are closed')
+console.log(rest.get('name'))
+console.log(rest.get('open'))
+console.log(rest.get(1))
+
+const time = 8;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')))
+
+console.log(rest.has('categories'))
+rest.delete(2)
+console.log(rest)
+console.log(rest.size)
+rest.clear()
+// rest.set([1, 2], 'Test')
+// console.log(rest)
+// console.log(rest.get([1, 2]))
+const testarr = [1, 2]
+rest.set(testarr, 'Test')
+// rest.set(document.querySelector('h1'), 'Heading')
+console.log(rest)
+
+//Map iterables
+
+const question = new Map(
+    [['question', 'What is the best programming lang in world?'],
+    [1, 'C'],
+    [2, 'Java'],
+    [3, 'JavaScript'],
+    ['correct', 3],
+    [true, 'correct'],
+    [false, 'Try again']]
+)
+console.log(question)
+
+// convert object to map
+
+console.log(Object.entries(openingHours))
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap)
+
+//Quiz app
+// const answer = Number(prompt('Your answer'))
+const answer = 3
+for (const [key, value] of question) {
+    if (typeof (key) === 'number') { console.log(`Answer ${key}:${value}`) }
+
+}
+
+console.log(answer)
+if (question.get('correct') === answer) {
+    console.log(question.get(true))
+} else {
+    console.log(question.get(false))
+}
+
+//convert map to an array
+console.log([...question])
